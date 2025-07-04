25b8689c3a3e5c09bc5facff995924039de94e47
Jack Lizard <muhammadievalbert171099@gmail.com>
2025-07-04 16:19:17 +0400

    update group_rights

--- group_rights.py
+++ group_rights.py
@@ -5,6 +5,18 @@ from aiogram import Router
 group = Router()
 
 @group.message()
-async def main(message: Message):
+async def join_chat_member(message: Message):
     if ContentType.NEW_CHAT_MEMBERS:
+        await message.delete()
+        await message.answer(f"Привет, {message.from_user.username}")
+
+@group.message()
+async def left_chat_member(message: Message):
+    if ContentType.LEFT_CHAT_MEMBER:
+        await message.delete()
+        await message.answer(f"Пользователь {message.from_user.username} покинул группу")
+
+@group.message()
+async def main(message: Message):
+    if ContentType:
         await message.answer(f"hello, {message.from_user.username}")
