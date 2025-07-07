d45f70bf3dd31060d27614abd6f1d3c680e5ce3b
Jack Lizard <muhammadievalbert171099@gmail.com>
2025-07-07 14:35:09 +0400

    update group_rights.py. Now, his name events.py

---
+++ events.py
@@ -0,0 +1,22 @@
+from aiogram.types import Message
+from aiogram.enums.content_type import ContentType
+from aiogram import Router
+
+event = Router()
+
+@event.message()
+async def join_chat_member(message: Message):
+    if ContentType.NEW_CHAT_MEMBERS:
+        await message.delete()
+        await message.answer(f"Привет, {message.from_user.username}")
+
+@event.message()
+async def left_chat_member(message: Message):
+    if ContentType.LEFT_CHAT_MEMBER:
+        await message.delete()
+        await message.answer(f"Пользователь {message.from_user.username} покинул группу")
+
+@event.message()
+async def main(message: Message):
+    if ContentType:
+        await message.answer(f"hello, {message.from_user.username}")
