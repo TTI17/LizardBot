9548b1a7dc9b46a9e76adab28e59f96bd38c9015
Jack Lizard <muhammadievalbert171099@gmail.com>
2025-08-07 01:30:14 +0300

    upgrade for security

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import asyncio
 import logging, sys
 from aiogram import Bot, Dispatcher
-from config import TOKEN
 from start import chat_router
 from events import event
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
 dp = Dispatcher()
+
 async def main() -> None:
     dp.include_router(chat_router)
     dp.include_router(event)
