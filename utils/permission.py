e85b52645b868dbf566c244135a73362dada7ed1
lizard <muhammadievalbert171099@gmail.com>
2025-09-08 15:06:44 +0400

    add checker member status for permissions to using some commands

--- utils/permission.py
+++ utils/permission.py
@@ -4,3 +4,7 @@ from aiogram.enums.chat_member_status import ChatMemberStatus
 async def is_admin(user_id: int, chat_id: int) -> bool:
     member = await bot.get_chat_member(chat_id=chat_id, user_id=user_id)
     return member.status in {ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.CREATOR}
+
+async def is_member(user_id: int, chat_id: int) -> bool:
+    member = await bot.get_chat_member(chat_id=chat_id, user_id=user_id)
+    return member.status in {ChatMemberStatus.MEMBER, ChatMemberStatus.ADMINISTRATOR, ChatMemberStatus.CREATOR}
