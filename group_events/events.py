59febab92f5a1ed4bf8495026da4b6bcda19dc20
lizard <muhammadievalbert171099@gmail.com>
2025-12-30 01:14:28 +0400

    delete functions with add the data to db

--- group_events/events.py
+++ group_events/events.py
@@ -10,29 +10,6 @@ from keyboards import get_rules_keyboard
 
 events = Router(name="groupEvents")
 
-# @events.message()
-# async def getMessage(message:Message):
-#     if message.chat.type in ["group", "supergroup"]:
-#         await getMessagesByUser(chat_id=message.chat.id, 
-#                                 user_id=message.from_user.id, 
-#                                 message_text=message.text)
-        
-#         add_message(chat_id=message.chat.id, 
-#                     user_id=message.from_user.id, 
-#                     text=message.text, 
-#                     username=message.from_user.username, 
-#                     chat_title=message.chat.title)
-#     else:
-#         return
-  
-# @events.message(F.text)
-# async def getUser(message:Message):
-#     if message.chat.type in ["group", "supergroup"]:
-#         await add_user(getInfoAboutUser(chat_id=message.chat.id, 
-#                             user_id=message.from_user.id))
-#     else:
-#         return
-
 @events.chat_member()
 async def handle_chat_member_update(event: ChatMemberUpdated):
     """
